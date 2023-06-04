55a1d562ce3a6356197b9da54e847e58578f4ba8
Sabin Lehaci <57048754+saboel@users.noreply.github.com>
2023-06-04 18:14:31 -0500

    added all project files

---
+++ mcj_supply/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace mcj_supply
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(353, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(320, 237);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(145, 17);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 210);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Please enter a Load-ID";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(294, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(194, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(820, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AGV Target";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ mcj_supply/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace mcj_supply
+{
+    public partial class Form1 : Form
+    {
+        private string txtFilesPath = "./";
+        private Dictionary<string, string> fileContentsCache = new Dictionary<string, string>();
+        private string latestFilePath = string.Empty;
+        private FileSystemWatcher fileWatcher;
+
+
+
+
+
+        public Form1() //constructor 
+        {
+            InitializeComponent();
+            Console.WriteLine(txtFilesPath); //debugging purposes: 
+            InitializeFileWatcher();
+            GetLatestTextFilePath();
+        }
+        private void Form1_Load(object sender, EventArgs e) //dont know what this is: usually empty unless you want default values populated when the form window loads
+        {
+
+        }
+
+        private void InitializeFileWatcher()
+        {
+            fileWatcher = new FileSystemWatcher
+            {
+                Path = txtFilesPath,
+                Filter = "*.txt",
+                EnableRaisingEvents = true
+            };
+
+            // Subscribe to the events
+            fileWatcher.Created += FileWatcher_Created;
+        }
+
+        private void FileWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            // A new text file was created
+            string filePath = e.FullPath;
+
+            if (string.IsNullOrEmpty(latestFilePath) || File.GetLastWriteTime(filePath) > File.GetLastWriteTime(latestFilePath))
+            {
+                latestFilePath = filePath;
+                GetLatestTextFilePath();
+            }
+        }
+
+     
+        private void GetLatestTextFilePath()
+        {
+            DirectoryInfo directory = new DirectoryInfo(txtFilesPath);
+            FileInfo[] textFiles = directory.GetFiles("*.txt");
+
+            if (textFiles.Length > 0)
+            {
+                FileInfo latestFile = textFiles.OrderByDescending(f => f.LastWriteTime).First();
+                //then first check if we have that latest file in the map! if we do then grab it from map
+                latestFilePath = latestFile.FullName;
+                if (!fileContentsCache.ContainsKey(latestFilePath))
+                {
+                    loadlatest(latestFilePath);
+                }
+            }
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string id = richTextBox1.Text;
+            // Load the latest text file
+
+            if (!string.IsNullOrEmpty(latestFilePath))
+            {
+                string target = GetTargetForLoadId(id);
+                if (target != null)
+                {
+                    label1.Text = "Target: " + target;
+                }
+                else
+                {
+                    label1.Text = "Load ID not found";
+                }
+            }
+            else
+            {
+                label1.Text = "No text files found";
+            }
+
+        }
+
+        private void loadlatest(string filepath)
+        {
+                string Filecontents = File.ReadAllText(filepath);
+                fileContentsCache[filepath] = Filecontents;
+              
+        }
+
+
+
+        private string GetTargetForLoadId(string loadId)
+        {
+            if (!string.IsNullOrEmpty(latestFilePath) && fileContentsCache.ContainsKey(latestFilePath))
+            {
+                string contents = fileContentsCache[latestFilePath];
+                string[] lines = contents.Split('\n');
+
+                foreach (string line in lines)
+                {
+                    string[] data = line.Split(';');
+                    if (data.Length >= 2 && data[0] == loadId)
+                    {
+                        return data[1]; // Return the target associated with the load ID
+                    }
+                }
+
+            }
+
+            return null; // Load ID not found
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+    }
+}
